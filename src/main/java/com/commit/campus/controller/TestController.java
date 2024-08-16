8d1e07d4e5c932ad423fccd7b953390096ec6c12
antmrhdqn <antmrhdqn@gmail.com>
2024-08-16 09:26:56 +0900

    테스트

--- src/main/java/com/commit/campus/controller/TestController.java
+++ src/main/java/com/commit/campus/controller/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("test")
 @Slf4j
 public class TestController {
 
