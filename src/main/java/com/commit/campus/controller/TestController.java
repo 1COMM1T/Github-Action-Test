600e60b9a736188c6da748ca9308edf96b422739
antmrhdqn <antmrhdqn@gmail.com>
2024-08-16 09:20:33 +0900

    브랜치 테스트

---
+++ src/main/java/com/commit/campus/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.commit.campus.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestController {
+
+    @PostMapping
+    public ResponseEntity<Void> test() {
+        log.info("확인");
+        return ResponseEntity.ok().build();
+    }
+}
